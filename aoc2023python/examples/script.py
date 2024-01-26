1ae16748f3d14378df94d6bda03546a455ad4a20
Sonia Mitchell <soniamitchell@gmail.com>
2024-01-26 14:35:46 +0000

    initial commit

---
+++ aoc2023python/examples/script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# Day 1: Trebuchet?!
+
+day01("aoc2023python/data/day01a.txt")
+day01("aoc2023python/data/day01b.txt")
+
+# Day 2: Cube Conundrum
+
+day02("aoc2023python/data/day02.txt")
+day02("aoc2023python/data/day02.txt")
+
+# Day 3: Gear Ratios
+
+day03("aoc2023python/data/day03.txt")
+day03b("aoc2023python/data/day03.txt")
+
+# Day 4: Scratchcards
+
+day04("aoc2023python/data/day04.txt")
+day04b("aoc2023python/data/day04.txt")
+
+# Day 5: If You Give A Seed A Fertilizer
